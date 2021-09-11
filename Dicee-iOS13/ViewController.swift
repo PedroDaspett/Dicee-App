43b7b7e8edf21ce46b504775deff6a984d294f98
Pedro Couventaris Daspett <pedrodaspett@hotmail.com>
2021-09-11 04:27:13 -0300

    Adding app  files

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        //These are two ways of choosing random elements
+        diceImageView1.image =  diceArray.randomElement()
+        diceImageView2.image =  diceArray[Int.random(in: 0...5)]
+        
+        
     }
-
-
+    
 }
 
